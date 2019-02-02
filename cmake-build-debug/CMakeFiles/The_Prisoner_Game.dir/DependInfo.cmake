57350fdbde891ae731a37e66d6435a239f974c89
alethecine96 <alessandro.raspanti@stud.unifi.it>
2019-02-02 14:53:45 +0100

    add folder

---
+++ cmake-build-debug/CMakeFiles/The_Prisoner_Game.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alessandro/CLionProjects/The_Prisoner_Game/Drawable.cpp" "/home/alessandro/CLionProjects/The_Prisoner_Game/cmake-build-debug/CMakeFiles/The_Prisoner_Game.dir/Drawable.cpp.o"
+  "/home/alessandro/CLionProjects/The_Prisoner_Game/Enemy.cpp" "/home/alessandro/CLionProjects/The_Prisoner_Game/cmake-build-debug/CMakeFiles/The_Prisoner_Game.dir/Enemy.cpp.o"
+  "/home/alessandro/CLionProjects/The_Prisoner_Game/Eyes.cpp" "/home/alessandro/CLionProjects/The_Prisoner_Game/cmake-build-debug/CMakeFiles/The_Prisoner_Game.dir/Eyes.cpp.o"
+  "/home/alessandro/CLionProjects/The_Prisoner_Game/Inventory.cpp" "/home/alessandro/CLionProjects/The_Prisoner_Game/cmake-build-debug/CMakeFiles/The_Prisoner_Game.dir/Inventory.cpp.o"
+  "/home/alessandro/CLionProjects/The_Prisoner_Game/MapLoader.cpp" "/home/alessandro/CLionProjects/The_Prisoner_Game/cmake-build-debug/CMakeFiles/The_Prisoner_Game.dir/MapLoader.cpp.o"
+  "/home/alessandro/CLionProjects/The_Prisoner_Game/Mappa.cpp" "/home/alessandro/CLionProjects/The_Prisoner_Game/cmake-build-debug/CMakeFiles/The_Prisoner_Game.dir/Mappa.cpp.o"
+  "/home/alessandro/CLionProjects/The_Prisoner_Game/Observer.cpp" "/home/alessandro/CLionProjects/The_Prisoner_Game/cmake-build-debug/CMakeFiles/The_Prisoner_Game.dir/Observer.cpp.o"
+  "/home/alessandro/CLionProjects/The_Prisoner_Game/Player.cpp" "/home/alessandro/CLionProjects/The_Prisoner_Game/cmake-build-debug/CMakeFiles/The_Prisoner_Game.dir/Player.cpp.o"
+  "/home/alessandro/CLionProjects/The_Prisoner_Game/Projectile.cpp" "/home/alessandro/CLionProjects/The_Prisoner_Game/cmake-build-debug/CMakeFiles/The_Prisoner_Game.dir/Projectile.cpp.o"
+  "/home/alessandro/CLionProjects/The_Prisoner_Game/main.cpp" "/home/alessandro/CLionProjects/The_Prisoner_Game/cmake-build-debug/CMakeFiles/The_Prisoner_Game.dir/main.cpp.o"
+  "/home/alessandro/CLionProjects/The_Prisoner_Game/menu.cpp" "/home/alessandro/CLionProjects/The_Prisoner_Game/cmake-build-debug/CMakeFiles/The_Prisoner_Game.dir/menu.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/The_Prisoner_Game.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/The_Prisoner_Game.dir/main.cpp.o"
+  "CMakeFiles/The_Prisoner_Game.dir/menu.cpp.o"
+  "CMakeFiles/The_Prisoner_Game.dir/Inventory.cpp.o"
+  "CMakeFiles/The_Prisoner_Game.dir/Mappa.cpp.o"
+  "CMakeFiles/The_Prisoner_Game.dir/Player.cpp.o"
+  "CMakeFiles/The_Prisoner_Game.dir/Eyes.cpp.o"
+  "CMakeFiles/The_Prisoner_Game.dir/MapLoader.cpp.o"
+  "CMakeFiles/The_Prisoner_Game.dir/Projectile.cpp.o"
+  "CMakeFiles/The_Prisoner_Game.dir/Enemy.cpp.o"
+  "CMakeFiles/The_Prisoner_Game.dir/Drawable.cpp.o"
+  "CMakeFiles/The_Prisoner_Game.dir/Observer.cpp.o"
+  "The_Prisoner_Game.pdb"
+  "The_Prisoner_Game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/The_Prisoner_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
